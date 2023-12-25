9238525d3e0a92414cadcaddacafce02a592037b
Sid <siddharthbisht99@gmail.com>
2023-12-25 13:35:43 +0530

    basic file read

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io::{self, Write};
-
+use std::{
+    fs,
+    io::{self, ErrorKind, Write},
+};
 fn read_string() -> String {
     let mut input = String::new();
     std::io::stdin()
@@ -7,7 +9,30 @@ fn read_string() -> String {
         .expect("Could not read the input!!");
     input
 }
+fn decode_bencoded_file(file_path: String) -> anyhow::Result<()> {
+    println!("Reading file at {file_path}");
+    let file_data = fs::read(&file_path);
+    match file_data {
+        Ok(_) => {
+            println!("Will try to decode this");
+            Ok(())
+        }
+        Err(e) => {
+            println!("{e:?}");
+            if e.kind() == ErrorKind::NotFound {
+                println!("File path does not exist, check the file path again!!");
+            } else {
+                println!("Could not read the file!!");
+            }
+            Err(e.into())
+        }
+    }
+}
 
+fn download_using_file(file_path: String) -> anyhow::Result<()> {
+    decode_bencoded_file(file_path)?;
+    Ok(())
+}
 fn main() {
     println!(
         r"
@@ -22,15 +47,20 @@ ______          _          ______ _ _
 "
     );
 
-    println!("What would you like to do:\n    1) Download using .torrent file\n    2) Download using magnet link\n    3) Quit Rusty-Bit\n");
     loop {
-        print!("Choose your preffered download method or quit the program: ");
+        println!("What would you like to do:\n    1) Download using .torrent file\n    2) Download using magnet link\n    3) Quit Rusty-Bit\n");
+        print!("Choose your preferred download method or quit the program: ");
         io::stdout().flush().expect("Couldn't flush stdout");
         let chosen_option = read_string();
         match chosen_option.trim() {
             "1" => {
-                println!("You chose to download using .torrent file");
-                break;
+                print!("You chose to download using .torrent file, provide the file path: ");
+                io::stdout().flush().expect("Couldn't flush stdout");
+                let file_path = read_string();
+                let result = download_using_file(file_path.trim().to_owned());
+                if result.is_ok() {
+                    break;
+                }
             }
             "2" => {
                 println!("You chose to download using magnet link");
