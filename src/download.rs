ebb1c15426db1c91eeebf2d30a2a3dacf71b1bc6
Sid <siddharthbisht99@gmail.com>
2023-12-26 12:45:50 +0530

    basic test (passing)

--- src/download.rs
+++ src/download.rs
@@ -9,8 +9,9 @@ fn decode_bencoded_file(file_path: String) -> anyhow::Result<()> {
     println!("Trying to read file {file_path}");
     let file_data = fs::read(&file_path);
     match file_data {
-        Ok(_) => {
-            println!("Will try to decode this");
+        Ok(file_data_vec) => {
+            println!("Decoding bencoded file {file_path}");
+            print!("{}", String::from_utf8_lossy(&file_data_vec));
             Ok(())
         }
         Err(e) => {
@@ -30,4 +31,21 @@ pub fn download_using_file() -> anyhow::Result<()> {
     let file_path = read_string();
     decode_bencoded_file(file_path)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod download_test {
+    use crate::download::decode_bencoded_file;
+
+    #[test]
+    fn check_decode_becoded_file_fails() {
+        let file_path: String = "non exisitng file path".to_string();
+        assert!(decode_bencoded_file(file_path).is_err());
+    }
+
+    #[test]
+    fn check_decode_becoded_file_passes() {
+        let file_path: String = r"C:\Users\SIDDHARTH\Desktop\sample.torrent".to_string();
+        assert!(decode_bencoded_file(file_path).is_ok());
+    }
 }
