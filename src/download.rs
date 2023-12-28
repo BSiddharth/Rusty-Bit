cc302dda751320810902d495d8996ed66cd67718
Sid <siddharthbisht99@gmail.com>
2023-12-28 12:31:06 +0530

    changed recursive decoder return type and added functionality and test for handling vec

--- src/download.rs
+++ src/download.rs
@@ -8,6 +8,7 @@ use std::{
     usize,
 };
 
+#[allow(dead_code)]
 #[derive(PartialEq, Debug)]
 enum BencodeType {
     Bstring(String),
@@ -15,31 +16,64 @@ enum BencodeType {
     Bmap(BTreeMap<Box<BencodeType>, Box<BencodeType>>),
     Bint(u64),
 }
-fn recursive_bencode_decoder(data: &Vec<u8>) -> anyhow::Result<BencodeType> {
-    let mut buffer: String = String::new();
+#[allow(dead_code)]
+fn recursive_bencode_decoder(data: &Vec<u8>) -> anyhow::Result<(BencodeType, usize)> {
     let mut index: usize = 0;
 
     // if index is out of range for data at any point, error will be thrown which is expected
     let mut data_char: char = data[index] as char;
+    println!("Char is {data_char}");
     if data_char.is_ascii_digit() {
+        println!("Entering digit mode");
+        let mut len_string: String = String::new();
         while data_char != ':' {
-            buffer.push(data_char);
+            len_string.push(data_char);
             index += 1;
             data_char = data[index] as char;
         }
-        return Ok(BencodeType::Bstring(String::from_utf8(
-            data[index + 1..index + 1 + buffer.parse::<usize>()?].to_vec(),
-        )?));
+
+        let result_string =
+            String::from_utf8(data[index + 1..index + 1 + len_string.parse::<usize>()?].to_vec())?;
+        let consumed = result_string.len() + len_string.len() + 1; // +1 for ':'
+        println!("returning {result_string}, {consumed}");
+        return Ok((BencodeType::Bstring(result_string), consumed));
     } else if data_char == 'i' {
         index += 1;
         data_char = data[index] as char;
+        let mut buffer: String = String::new();
         while data_char != 'e' {
             buffer.push(data_char);
             index += 1;
             data_char = data[index] as char;
         }
-        return Ok(BencodeType::Bint(buffer.parse::<u64>()?));
+
+        let result_int = BencodeType::Bint(buffer.parse::<u64>()?);
+        let consumed = buffer.len() + 2; // +2 for 'i' and 'e'
+
+        return Ok((result_int, consumed));
+    } else if data_char == 'l' {
+        println!("Entering l mode");
+        let mut ben_vec: Vec<BencodeType> = Vec::new();
+        while data[index] as char != 'e' {
+            let data_to_send_start_index: usize = if ben_vec.len() == 0 { 1 } else { 0 };
+            let (list_element, new_index) =
+                recursive_bencode_decoder(&data[index + data_to_send_start_index..].to_vec())?;
+            index += if ben_vec.len() == 0 {
+                new_index + 1
+            } else {
+                new_index
+            };
+            ben_vec.push(list_element);
+        }
+
+        println!("{ben_vec:?}");
+
+        let consumed = ben_vec.len() + 2; // +2 for 'i' and 'e'
+        let result_vec = BencodeType::Bvec(ben_vec);
+
+        return Ok((result_vec, consumed));
     }
+
     println!("Could not parse the bencoded data properly!");
     bail!("Could not parse the bencoded data properly!")
 }
@@ -92,14 +126,23 @@ mod download_test {
     fn check_recursive_bencode_decoder_passes() {
         let str_vec: &Vec<u8> = &String::from("11:Test String").into_bytes();
         assert_eq!(
-            recursive_bencode_decoder(str_vec).unwrap(),
+            recursive_bencode_decoder(str_vec).unwrap().0,
             BencodeType::Bstring(String::from("Test String"))
         );
 
         let int_vec: &Vec<u8> = &String::from("i982e").into_bytes();
         assert_eq!(
-            recursive_bencode_decoder(int_vec).unwrap(),
+            recursive_bencode_decoder(int_vec).unwrap().0,
             BencodeType::Bint(982)
+        );
+
+        let vec_vec: &Vec<u8> = &String::from("l4:spam4:eggse").into_bytes();
+        assert_eq!(
+            recursive_bencode_decoder(vec_vec).unwrap().0,
+            BencodeType::Bvec(vec![
+                BencodeType::Bstring(String::from("spam")),
+                BencodeType::Bstring(String::from("eggs"))
+            ])
         );
     }
 
@@ -108,7 +151,7 @@ mod download_test {
     fn check_recursive_bencode_decoder_string_len_fails() {
         let str_vec: &Vec<u8> = &String::from("12:Test String").into_bytes();
         assert_eq!(
-            recursive_bencode_decoder(str_vec).unwrap(),
+            recursive_bencode_decoder(str_vec).unwrap().0,
             BencodeType::Bstring(String::from("Test String"))
         );
     }
@@ -118,7 +161,7 @@ mod download_test {
     fn check_recursive_bencode_decoder_int_no_end_fails() {
         let int_vec: &Vec<u8> = &String::from("i32").into_bytes();
         assert_eq!(
-            recursive_bencode_decoder(int_vec).unwrap(),
+            recursive_bencode_decoder(int_vec).unwrap().0,
             BencodeType::Bstring(String::from("Should Panic"))
         );
     }
@@ -128,7 +171,28 @@ mod download_test {
     fn check_recursive_bencode_decoder_deformed_fails() {
         let data_vec: &Vec<u8> = &String::from("qi23e").into_bytes();
         assert_eq!(
-            recursive_bencode_decoder(data_vec).unwrap(),
+            recursive_bencode_decoder(data_vec).unwrap().0,
+            BencodeType::Bstring(String::from("Should Panic"))
+        );
+    }
+
+    #[should_panic]
+    #[test]
+    fn check_recursive_bencode_decoder_vec_no_end_fails() {
+        let vec_vec: &Vec<u8> = &String::from("l4:spam4:eggs").into_bytes();
+        assert_eq!(
+            recursive_bencode_decoder(vec_vec).unwrap().0,
+            BencodeType::Bstring(String::from("Should Panic"))
+        );
+    }
+
+    #[should_panic]
+    #[test]
+    fn check_recursive_bencode_decoder_vec_bad_str_fails() {
+        let vec_vec: &Vec<u8> = &String::from("l4:spam3:eggs").into_bytes(); // eggs len should be
+                                                                             // 4
+        assert_eq!(
+            recursive_bencode_decoder(vec_vec).unwrap().0,
             BencodeType::Bstring(String::from("Should Panic"))
         );
     }
